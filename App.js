37a9615ccf3dbcb8680b7896732d316310dd42ac
manumaestro <manuela.maestro@aluno.senai.br>
2026-02-13 09:56:23 -0300

    Finaliza a tela de login

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     <View style={styles.header}>
       <View style={styles.boxLogo}>
         <Image
-        source={require("./assets/capela.png")}
+        source={require("./assets/capelo.png")}
         style={styles.logo} />
       </View>
 
@@ -23,18 +23,18 @@ export default function App() {
       <View style={styles.inputArea}>
         <Ionicons name="mail-outline" size={20} color="#888" />
         <TextInput
-        styles={styles.input}
+        style={styles.input}
         placeholder="seu@email.com"/>
       </View>
 
       <Text style={styles.label}>Senha</Text>
       <View style={styles.inputArea}>
-        <Ionicons name="cadeado-outline" size={20} color="#888" />
+        <Ionicons name="lock-closed-outline" size={20} color="#888" />
         <TextInput 
         style={styles.input}
         placeholder="********"
         secureTextEntry />
-        <Ionicons name="olho-outline" size={20} coor="#888" />
+        <Ionicons name="eye-outline" size={20} color="#888" />
       </View>
 
       <View style={styles.esqueciArea}>
@@ -46,8 +46,31 @@ export default function App() {
       </TouchableOpacity>
     </View>
 
-    
+    <View style={styles.divisor}>
+      <View style={styles.linha} />
+      <Text style={styles.textoCinza}>Ou entre com</Text>
+      <View style={styles.linha} />
     </View>
+
+    <View style={styles.social}>
+      <TouchableOpacity style={styles.socialBotao}>
+        <FontAwesome name="google" size={20} color="#d43131" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.socialBotao}>
+        <FontAwesome name="github" size={20} color="#000" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.socialBotao}>
+        <FontAwesome name="linkedin" size={20} color="#373add" />
+      </TouchableOpacity>
+    </View>
+
+    <View style={styles.footer}>
+      <Text style={styles.textoCinza}>Não tem uma conta?</Text>
+      <Text style={styles.textoRoxo}>Cadastrar-se</Text>
+    </View>
+</View>
 
   );
 }
@@ -56,7 +79,129 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
   },
+
+  header: {
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  
+  boxLogo: {
+    width: 90,
+    height: 90,
+    borderRadius: 20,
+    backgroundColor:"#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#74129b",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 12,
+  },
+
+  logo: {
+    width: 65,
+    height: 65,
+    resizeMode: "contain",
+  },
+
+  titulo: {
+    fontSize: 22,
+    fontWeight: "600",
+    marginTop: 15,
+  },
+
+  subtitulo: {
+    color: "#777",
+    marginTop: 5,
+  },
+
+  form: {
+    paddingHorizontal: 30,
+  },
+
+  inputArea: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1.5,
+    borderColor: "#e5e5e5",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    height: 45,
+    marginBottom: 15,
+  },
+
+  input: {
+    flex: 1,
+    marginLeft: 8,
+  },
+
+  esqueciArea: {
+    alignItems: "flex-end",
+    marginBottom: 20,
+  },
+
+  textoRoxo: {
+    color: "#7b2ad8",
+    fontWeight: "600",
+  },
+
+  botao: {
+    backgroundColor: "#7b2ad8",
+    height: 48,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 8,
+  },
+
+  textoBotao: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+
+  divisor: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 30,
+    marginTop: 30,
+  },
+
+  linha: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#ddd",
+  },
+
+  textoCinza: {
+    color: "#777",
+    marginHorizontal: 10,
+  },
+
+  social: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 25,
+    gap: 25,
+  },
+
+  socialBotao: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 4,
+  },
+
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+
 });
